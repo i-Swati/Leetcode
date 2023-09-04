2090d8ff07c9566c8ce699eb82c45cb5c3a4908d
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-04 20:45:37 +0530

    Time: 21 ms (77.95%), Space: 25.1 MB (45.89%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     void rotate(vector<int>& arr, int d) {
-    int n=arr.size();
-
-    vector<int>temp(n);
-    for(int i=0; i<n; i++){
-        temp[(i+d)%n]=arr[i];
-    }
-    arr=temp;
+        int n = arr.size();
+        d = d % n; // Calculate the effective rotation value
+        
+        // Reverse the entire array
+        reverse(arr.begin(), arr.end());
+        
+        // Reverse the first part
+        reverse(arr.begin(), arr.begin() + d);
+        
+        // Reverse the second part
+        reverse(arr.begin() + d, arr.end());
     }
 };
