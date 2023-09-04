b1a4820cb6ea444feca55a15e8b64d26cfb9fe9c
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-04 19:31:26 +0530

    Time: 23 ms (70.82%), Space: 25.6 MB (29.07%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& arr, int d) {
-        int n = arr.size();
+    int n=arr.size();
 
-        d = d % n;
-
-        vector<int> temp(d); // Initialize temp with the same size as d
-
-        // Copy the last 'd' elements to temp
-        for (int i = 0; i < d; i++) {
-            temp[i] = arr[n - d + i];
-        }
-
-        // Shifting
-        for (int i = n - 1; i >= d; i--) {
-            arr[i] = arr[i - d];
-        }
-
-        // Copy the elements from temp to the beginning of arr
-        for (int i = 0; i < d; i++) {
-            arr[i] = temp[i];
-        }
+    vector<int>temp(n);
+    for(int i=0; i<n; i++){
+        temp[(i+d)%n]=arr[i];
+    }
+    arr=temp;
     }
 };
