7bb4f7526041aa46d8c6bcccabdc9a006425e860
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-11 15:04:05 +0530

    Time: 19 ms (87.61%), Space: 24.8 MB (91.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     void rotate(vector<int>& arr, int d) {
         int n = arr.size();
@@ -12,5 +12,28 @@ public:
         
         // Reverse the second part
         reverse(arr.begin() + d, arr.end());
+    }
+};*/
+class Solution{
+    public:
+    void reverse(vector<int>&arr, int low, int high){
+        while(low<high){
+        int temp=arr[low];
+        arr[low]=arr[high];
+        arr[high]=temp;
+
+        low++;
+        high--;
+        }
+    }
+
+    void rotate(vector<int>&arr, int d){
+        int n=arr.size();
+        d=d%n;
+
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        
     }
 };
