368db10e9a978cf95292034bda9cbc46a4db82e8
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-09-30 21:27:49 +0530

    Time: 7 ms (97.64%), Space: 19.9 MB (21.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-//BETTER SOLUTION
-    int majorityElement(vector<int>& arr) { 
-        map<int, int>mpp;
-        for(int i=0; i<arr.size(); i++){ //O(n log n)
-            mpp[arr[i]]++;
-
-        }
-        for(auto it:mpp){ //O(n)
-            if(it.second>arr.size()/2){
-                return it.first;
+    int majorityElement(vector<int>& arr) {
+        int elem=0;
+        int cnt=0;
+        for(int i=0; i<arr.size();i++){
+            if(cnt==0){
+                cnt=1;
+                elem=arr[i];
+            }
+            else if(arr[i]==elem){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
-        return -1;
-        
+        return elem;
     }
 };
