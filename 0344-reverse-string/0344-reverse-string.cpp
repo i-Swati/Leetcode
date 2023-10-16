889ac03302ebf690b7af30ab62b29f18fc4795a9
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-10-16 17:18:04 +0530

    Time: 19 ms (52.36%), Space: 23.7 MB (7.72%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -5,11 +5,7 @@ public:
         int high=s.size()-1;
 
         while(low<high){
-            char temp=s[low];
-            s[low]=s[high];
-            s[high]=temp;
-            low++;
-            high--;
+            swap(s[low++], s[high--]);
         }
     }
 };
