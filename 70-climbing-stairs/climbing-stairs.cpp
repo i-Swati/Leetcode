48257a9ba712e36992b3e1a0f48c2dbce3f6d0af
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-11-24 23:25:19 +0530

    Time: 3 ms (12.30%) | Memory: 6.5 MB (32.44%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1);
+        //base case
+        if(n==1){
+            return 1;
+        }
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+//TC=O(n) //SC=O(n)
