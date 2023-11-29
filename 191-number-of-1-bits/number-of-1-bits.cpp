d72d3c6a6d72f1401f124e77f06ea6b43071150d
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-11-29 23:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (57.22%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int count=0;
         while(n!=0){
-            if(n&1){ //check if last bit is 1
-                count++;
+            if(n&1){ //check if last bit is 1; using AND operation
+            count++;
             }
             n=n>>1; //right shift
         }
