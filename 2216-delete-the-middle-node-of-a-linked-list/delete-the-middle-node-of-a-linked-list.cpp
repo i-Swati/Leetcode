775092eb822bd1b2eecc9d4a202c10e1d5fac776
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-12-01 19:32:53 +0530

    Time: 616 ms (22.17%) | Memory: 295.1 MB (70.66%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -1,47 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0; // Count/number of elements
+        int cnt=0;
+        ListNode* temp=head;
 
-        // Count the number of nodes in the linked list
-        while (temp != NULL) {
-            count++;
-            temp = temp->next;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
         }
-        cout<<count<<endl; //printed count to check/ for understanding
-        
 
-        temp = head; // Reset temp to head
-        int n; // Position of the middle node
+        temp=head;
+        int n=0;
 
-        if (count % 2 == 0) {
-            // For even count of elements, consider the second middle node
-            n = count / 2 +1; // For the second middle node in even cases
-        } else {
-            // For odd count of elements
-            n = (count + 1) / 2; // For the exact middle node in odd cases
+        if(cnt%2==0){
+            n=cnt/2 + 1;
         }
-        cout<<n; //for understanding
-        
-        ListNode* prev = NULL;
+        else{
+            n= (cnt+1)/2;
+        }
 
+        ListNode* prev=NULL;
+        
         // Traverse to the node before the middle node
         while (n > 1) {
             prev = temp;
             temp = temp->next;
             n--;
         }
-
-        // Deleting the middle node based on the adjusted position
-        if (prev != NULL) {
-            prev->next = temp->next; // Skip the middle node
-            delete temp; // Delete the middle node
-        } else {
-            head = head->next; // Update head to the node after the middle node
-            delete temp; // Delete the original head (middle node)
+        if(prev!=NULL){
+            prev->next=temp->next;
+            delete temp;
         }
-
+        else{
+            head=head->next;
+            delete temp;
+        }
         return head;
     }
 };
