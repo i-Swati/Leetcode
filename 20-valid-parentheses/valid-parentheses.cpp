adb15b16122cd34a4cfb46f631445fda9479732e
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2023-12-14 14:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (68.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+
+        for(int i=0; i<str.length(); i++){
+            char ch=str[i];
+
+            //if opening bracket, push in stack
+            //if closing bracket, check stack top and pop
+
+            if((ch=='(')|| (ch=='{')|| (ch=='[')){
+                s.push(ch);
+            }
+
+            else{
+                if(!s.empty()){
+                    char top=s.top();
+                    if((ch==')' && top=='(')|| (ch=='}' && top=='{')|| (ch==']' && top=='[')){
+                        s.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{ //stack is empty
+                    return false;
+                }
+            }
+        }
+        if(s.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
