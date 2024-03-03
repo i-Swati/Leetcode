73ec6a61622db0b29f8011af04b3f084adbbf228
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-03-04 00:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (55.19%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,32 +5,38 @@ public:
         // Handle the case of INT_MIN: Handling INT_MIN separately helps 
         //prevent integer overflow and ensures correct 
         //calculations for extreme cases.
-
+        
         if (n == INT_MIN) {
             x = x * x;
             n = n / 2;
         }
 
         //base case
-        if(n==0){
-            return 1.0;
-        } //ruk jao
+         if(n==0){
+             return 1.0;
+         }
+         if(n==1){
+             return x;
+         }
 
         //handle negative exponent
         if(n<0){ //negative number
             x=1/x;
             n=-n; //eg.- 2^-3=(1/2)^3=(1/2)^(n-2n)=(1/2)^(-n)
+
         }
 
-        //R.R
-        //handle all other positives
+        //if n is even
         
-        if (n % 2 == 0) { //halved to reduce TC
-            double halfPow = myPow(x, n / 2);
-            return halfPow * halfPow;
-            } 
-        else {
-            return x * myPow(x, n-1);
-            }
+        if(n%2==0){
+            //recursive call
+            double ans= myPow(x, n/2);
+            return ans*ans;
+        }
+
+        //if n is odd
+        else{
+            return x*myPow(x, n-1);
+        }
     }
 };
