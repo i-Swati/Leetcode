2fd28d7dbffaee8611f15ddf6a53d9a0c7ede585
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-03-10 10:56:03 +0530

    Time: 4 ms (68.61%) | Memory: 13.2 MB (26.85%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> intersection(vector<int>& arr1, vector<int>& arr2) {
         unordered_set<int> set1(arr1.begin(), arr1.end());
 
-        vector<int>arr;
-
+        vector<int> arr;
+        
         for(int i=0; i<arr2.size(); i++){
             if(set1.count(arr2[i])){
-                arr.push_back(arr2[i]);
-                set1.erase(arr2[i]);  // Remove the element from set1
+               arr.push_back(arr2[i]);
+               set1.erase(arr2[i]);
             }
         }
         return arr;
