6adb37d23ee2f0a3d562cd1d7b92f171c81a3a5a
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-03-14 14:44:47 +0530

    Time: 4 ms (90.24%) | Memory: 9.7 MB (16.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-//BRUTE FORCE
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
 
-        for(int i=0; i<s.length(); i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
+        unordered_map<char, int> charCount;
+
+        // Count characters in s
+        for (char c : s) {
+            charCount[c]++;
         }
+
+        // Subtract characters in t
+        for (char c : t) {
+            if (charCount.find(c) == charCount.end() || charCount[c] == 0) {
+                return false;  // Character not found or no more occurrences left
+            }
+            charCount[c]--;
+        }
+
         return true;
     }
 };
