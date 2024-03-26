14773858c0b8de5304f2e06a95e894724690c44d
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-03-26 08:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (60.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
-
 public:
     bool isValid(string str) {
         stack<char> s;
-
         for(int i=0; i<str.length(); i++){
             char ch=str[i];
 
             //if opening bracket, push in stack
             //if closing bracket, check stack top and pop
 
-            if((ch=='(')|| (ch=='{')|| (ch=='[')){
+            if((ch=='(') || (ch=='{') || (ch=='[') ){
                 s.push(ch);
             }
             else{
                 if(!s.empty()){
                     char top=s.top();
-                    if((ch==')' && top=='(')|| (ch=='}' && top=='{')|| (ch==']' && top=='[')){
+                    if((ch==')' && top=='(') || (ch=='}' && top=='{') || (ch==']' && top=='[')){
                         s.pop();
                     }
                     else{
