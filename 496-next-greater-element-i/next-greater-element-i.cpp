15d5ebcdcfd5938bca70ccb1590edddcef2b2ca9
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-04-21 21:01:47 +0530

    Time: 11 ms (14.25%) | Memory: 10.8 MB (81.56%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            bool found = false;
+            bool greaterFound = false;
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums1[i] == nums2[j]) {
+                    found = true;
+                }
+                if(found && nums2[j] > nums1[i]) {
+                    ans.push_back(nums2[j]);
+                    greaterFound = true;
+                    break;
+                }
+            }
+            if (!greaterFound)
+                ans.push_back(-1);
+        }
+        return ans;
+    }
+};
