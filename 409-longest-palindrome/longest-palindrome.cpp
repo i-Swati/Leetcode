d001ad926bdd057bc2184eec6318768b240b3134
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-06-04 23:16:14 +0530

    Time: 2 ms (63.38%) | Memory: 9 MB (48.94%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-            unordered_map<char, int> freq;
-    for (char c : s) {
-        freq[c]++;
-    }
-    
-    int length = 0;
-    bool odd_found = false;
-    
-    for (auto it = freq.begin(); it != freq.end(); it++) {
-    // Accessing the key and value separately
-    char key = it->first;
-    int value = it->second;
+        unordered_map<char, int> freq;
 
-    // Check if the frequency is even or odd
-    if (value % 2 == 0) {
-        length += value; // Add even counts directly
-    } else {
-        length += value - 1; // Add the largest even part of odd counts
-        odd_found = true; // Mark that we've seen at least one odd count
-    }
-}   
-    if (odd_found) {
-        length += 1;
-    }
-    
-    return length;
+        for (char c : s){
+            freq[c]++;
+        }
+
+        int length= 0;
+        bool odd_found= false;
+
+        for(auto it= freq.begin(); it!=freq.end(); it++){
+            char key= it->first;
+            int value= it->second;
+
+            if(value%2==0){
+                length= length + value;
+            }
+            else{
+                length= length + (value - 1);
+                odd_found= true;
+            }
+        }
+        if(odd_found){
+            length= length + 1;
+        }
+
+        return length;
     }
 };
