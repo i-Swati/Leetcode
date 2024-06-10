52563fe1f620be09199c034292b248714b437ef1
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-06-10 16:36:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (34.36%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
 //DP
-    int dp[101][101];
-    int solve(int i, int j, vector<vector<int>>& grid){
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
         if(i==0 && j==0)    return 1;
         if(i<0 || j<0)  return 0;
 
@@ -10,20 +9,22 @@ public:
 
         if(dp[i][j] != -1)  return dp[i][j];
         
-        int up= solve(i-1, j, grid);
-        int left= solve(i, j-1, grid);
+        int up= solve(i-1, j, grid, dp);
+        int left= solve(i, j-1, grid, dp);
 
         return dp[i][j]= up+left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        memset(dp, -1, sizeof(dp));
+         int n= grid.size(); //row size
+        int m= grid[0].size(); //column size
+
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
         // If the starting cell has an obstacle, return 0 immediately.
         if (grid[0][0] == 1) return 0;
 
-        int n= grid.size(); //row size
-        int m= grid[0].size(); //column size
-
         // Start the recursive function from the bottom-right corner.
-        return solve(n-1, m-1, grid);
+        return solve(n-1, m-1, grid, dp);
     }
 };
