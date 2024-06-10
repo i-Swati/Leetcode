6f29d0f76290249803818639b9cb89db5f8852b8
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-06-10 16:32:33 +0530

    Time: 3 ms (53.23%), Space: 9.9 MB (89.09%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//DP
     int dp[101][101];
     int solve(int i, int j, vector<vector<int>>& grid){
         if(i==0 && j==0)    return 1;
@@ -19,10 +20,10 @@ public:
         // If the starting cell has an obstacle, return 0 immediately.
         if (grid[0][0] == 1) return 0;
 
-        int i= grid.size();
-        int j= grid[0].size();
+        int n= grid.size(); //row size
+        int m= grid[0].size(); //column size
 
         // Start the recursive function from the bottom-right corner.
-        return solve(i-1, j-1, grid);
+        return solve(n-1, m-1, grid);
     }
 };
