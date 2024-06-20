21ea736b744bda9d4076a1d29ae51602cfbe8780
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-06-20 22:17:21 +0530

    Time: 17 ms (65.23%) | Memory: 15.9 MB (94.68%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    bool solve(int i,vector<vector<int>>& graph, bool visited[],int color[]){
-            visited[i]=true;
-            for(auto itr:graph[i]){
-// if uncolored color it with opposite color of its parent
-                if(color[itr]==-1){
-                    color[itr]=1-color[i];
-                }
-                else{
-// if it is colored with same color as of parent return it is not bipartite
-                    if(color[itr]!=1-color[i])
-                        return false;
-                }
-// Traverse for all the unvisited adjacent nodes and check
-                if(!visited[itr]){
-// If any one node fails to satisfy return false
-                    if(!solve(itr,graph,visited,color))
-                        return false;
-                }
+    bool dfs(int node, int col, vector<int> &colour, vector<vector<int>>& graph){
+        colour[node]= col;
+        for(auto &neigh: graph[node]){
+            if(colour[neigh]==-1){
+                if(dfs(neigh, !col, colour, graph)==false)   return false;;
             }
-// If all node satisfies return true
-            return true;
+            else if(colour[neigh]==col){
+                return false;
+            }
+        }
+        return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        int color[n];
-// Initialize color with -1
-        memset(color,-1,sizeof(color));
-        bool visited[n];
-// Initialize as unvisited
-        memset(visited,false,sizeof(visited));
-// Since nodes can be disconnected. It is possible that we are not able to reach all the nodes by starting from one node only. Thus check for all the nodes and if it is not visited process it.
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                color[i]=0;
-                if(!solve(i,graph,visited,color))
-                    return false;
+        int n= graph.size();
+        
+        vector<int> colour(n, -1);
+
+        //for disconnected graphs
+
+        for(int i= 0; i<n; i++){
+            if(colour[i]==-1){
+                if(dfs(i, 0, colour, graph)==false) return false;
             }
         }
         return true;
