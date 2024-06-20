e3e972ae4c7cc1d8ffae070b2732c9ba0d115595
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-06-20 13:07:11 +0530

    Time: 104 ms (86.14%) | Memory: 68.9 MB (65.57%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    int check(vector<int> &bloomDay, int k, int mid){
-        int bouq= 0;
-        int consec_cnt= 0;
+    int check(vector<int>& bloomDay, int k, int mid) {
+        int flower = 0;
+        int bouq = 0;
 
-        for(int i= 0; i<bloomDay.size(); i++){
+        for (int i = 0; i < bloomDay.size(); i++) {
             if(bloomDay[i]<=mid){
-                consec_cnt++;
+                flower++;
             }
             else{
-                consec_cnt= 0;
+                flower= 0;
             }
-
-            if(consec_cnt==k){
+            if(flower==k){
                 bouq++;
-                consec_cnt= 0;
+                flower= 0;
             }
         }
+
         return bouq;
     }
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n= bloomDay.size();
-        
-        int start_day=0;
-        int end_day= *max_element(begin(bloomDay), end(bloomDay));
+        int n = bloomDay.size();
+        // Use long long to avoid overflow
+        if ((long long)n < (long long)m * k) {
+            return -1; // Not enough flowers to form the required bouquets
+        }
 
-        int minDay= -1;
+        int minDay = *min_element(bloomDay.begin(), bloomDay.end());
+        int maxDay = *max_element(bloomDay.begin(), bloomDay.end());
 
-        //binary search
-        while(start_day<=end_day){
-            int mid= start_day + (end_day-start_day)/2;
+        int result = -1;
 
-            if(check(bloomDay, k, mid)>=m){
-                minDay= mid;
-                end_day= mid-1;
-            }
-            else{
-                start_day= mid + 1;
+        while (minDay <= maxDay) {
+            int mid = minDay + (maxDay - minDay) / 2;
+
+            if (check(bloomDay, k, mid)>=m) {
+                result = mid;
+                maxDay = mid - 1; // try for smaller days to minimize the days
+            } else {
+                minDay = mid + 1; // try for larger days
             }
         }
-        return minDay;
+        return result;
     }
 };
