f2bf50ab42b460e0e234e5ebe6f63789962975ec
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-08-04 22:45:55 +0530

    Time: 4 ms (65.23%) | Memory: 13 MB (14.81%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -13,34 +13,14 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* dummy= NULL;
 
-        while(head!= NULL){
+        while(head!=NULL){
             ListNode* next= head->next;
+
             head->next= dummy;
 
             dummy= head;
-            head= next;
+            head= next; //finally, we point head to the next pointer in the original ll
         }
         return dummy;
     }
 };
-/*I start by creating a pointer dummy which is initialized to NULL. This will eventually point to the head of the reversed list. Initially, it is NULL because the reversed list is empty at the beginning.
-
-I use a while loop to iterate through the entire list. The loop continues until head becomes NULL.
-
-Inside the loop, I perform several key operations:
-
-ListNode* next = head->next;
-First, I store the reference to the next node in a temporary variable next. This is important because I am going to change the head->next pointer, and I don't want to lose track of the rest of the list.
-head->next = dummy;
-Next, I reverse the link for the current node. Instead of pointing to the next node in the original list, the current node now points to the previous node (pointed to by dummy).
-dummy = head;
-Then, I move the dummy pointer to the current node. This means that dummy always points to the new head of the reversed list so far.
-head = next;
-Finally, I advance the head pointer to the next node in the original list (which I stored in next).
-
-
-
-After the loop completes, dummy points to the new head of the reversed list. I return dummy as the result.
-
-It runs in O(n) time complexity because it processes each node exactly once, and it uses O(1) space complexity because it only uses a few extra pointers
-*/
