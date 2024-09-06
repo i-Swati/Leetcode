f613296ef4b3bc70bb27112eefc108bc02c01483
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-09-06 10:59:58 +0530

    Time: 96 ms (52.08%) | Memory: 76.9 MB (82.36%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -4,10 +4,12 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
 //Approach 2- Better
 //SC- O(1)
+//[5, 2, 3, 4] k= 10;
     int n= chalk.size();
     long long sum= 0;
     for(int i= 0; i<n; i++){ //O(n)
         sum= sum + chalk[i];
+        if(sum > k) break; //14 > 10
     }
     //no. of full iterations possible, k/sum
 
