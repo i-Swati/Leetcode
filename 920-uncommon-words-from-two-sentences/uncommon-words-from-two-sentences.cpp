ae8bb7892191d40d8b8f5593e96b8b8fa6a46389
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-09-17 15:46:11 +0530

    Time: 5 ms (15.91%) | Memory: 9.2 MB (19.13%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        s1.push_back(' ');
+        s2.push_back(' ');
+
+        string temp= "";
+        unordered_map<string, int> mp;
+        vector<string> ans;
+        
+        for(int i= 0; i<s1.length(); i++){
+            if(s1[i]!=' '){
+                temp.push_back(s1[i]);
+            }
+            else{
+                mp[temp]= mp[temp] + 1;
+                temp= "";
+            }
+        }
+        for(int i= 0; i<s2.length(); i++){
+            if(s2[i]!=' '){
+                temp.push_back(s2[i]);
+            }
+            else{
+                mp[temp]= mp[temp] + 1;
+                temp= "";
+            }
+        }
+        for(auto i: mp){
+            if(i.second==1){
+                ans.push_back(i.first);
+            }
+        }
+        return ans;
+    }
+};
