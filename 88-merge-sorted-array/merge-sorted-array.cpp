e9e63d9be77237468995c4799876a092532e40b3
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-09-20 23:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (23.41%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0; i<n; i++){
-            nums1[m+i]=nums2[i];
+        vector<int> ans;
+        int i= 0; //nums1
+        int j= 0; //nums2
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
             }
-        sort(nums1.begin(), nums1.end());
-
+            else if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i]);
+                ans.push_back(nums2[j]);
+                i++;
+                j++;
+            }
+            else{
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<m){ //agar i m tak nahi pahuncha ho (matlab nums1 me elements bache hain)
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n){ //agar j n tak nhi pahuncha ho (matlab nums2 me elements bache hain)
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        for(int i= 0; i<m+n; i++){
+            nums1[i]=ans[i];
+        }
     }
 };
