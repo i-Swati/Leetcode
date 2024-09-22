c2b47a7fdd0da29ccbcdce46ec2688a4fbb739d1
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2024-09-22 13:18:55 +0530

    Time: 2 ms (40.16%) | Memory: 8.3 MB (45.80%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int revNum= 0;//x= 123
+
+        while(x!=0){
+        
+        int last_dig= x%10; //123 % 10= 3 
+
+        // Check for overflow before updating revNum
+        if (revNum > INT_MAX / 10 || (revNum == INT_MAX / 10 && last_dig > 7) || (revNum < INT_MIN/10)) {
+            return 0; // Return 0 if it would overflow
+        }
+
+        revNum= (revNum*10) + last_dig; //revNum= 3
+
+        x= x/10;
+        }            
+
+        /*if(isNeg){
+            return -revNum;
+        }*/
+        return revNum;      
+    }
+};
