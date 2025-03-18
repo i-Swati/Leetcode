b8024b97f90507a4280827c648df83d9ff4d1e71
Swati Kumari <137554521+i-Swati@users.noreply.github.com>
2025-03-18 10:57:25 +0530

    Time: 7 ms (19.68%) | Memory: 18.1 MB (48.93%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for(int num: nums){
+            freq[num]++;
+        }
+
+        for(auto &f: freq){
+            if(f.second % 2!= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
